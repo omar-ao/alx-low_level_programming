1dbadd34a907bf282bb35ab12aaf95bccfc5c35f
Omar Ahmed O <deeyndiid@gmail.com>
2023-07-13 15:48:29 +0300

    Add 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Calls the puts function to display
+ * "Programming is like building a multilingual puzzle
+ * to the standard output
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+}

40bf1fb0062586195ea6aa4493245a2843b5d1d9
Omar Ahmed O <deeyndiid@gmail.com>
2023-07-19 00:58:17 +0300

    Replace loop with recursion

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,6 +2,9 @@
 
 #define MAX_TERM 4000000 /* Max fibonacci term */
 
+unsigned long fibonacci(int);
+unsigned long sum_even_fibonacci(unsigned long, int, unsigned long);
+
 /**
  * main - Entry point
  * Description: Finds and prints the sum of the even valued terms
@@ -9,31 +12,41 @@
  */
 int main(void)
 {
-	unsigned long first, second, next, sum;
+	unsigned long result;
 
-	first = 1, second = 2;
-
-	sum = 2;
-
-	for ( ;next < MAX_TERM; )
-	{
-		next = first + second;
-		if (next % 2 == 0)
-			sum = sum + next;
-		first = second;
-		second = next;
-	}
-
-	printf("%lu\n", sum);
+	result = sum_even_fibonacci(MAX_TERM, 1, 0);
+	printf("%lu\n", result);
 	return (0);
 }
-unsigned long fibonacci(unsigned long);
 
-unsigned long sum_even_fibonacci(int limit, int n, unsigned long sum)
+/**
+ * fibonacci - Find fibonacci of n
+ * @n: Integer
+ * Return: Fibonacci of n
+ */
+unsigned long fibonacci(int n)
 {
-	unsigned long first, second, next, sum;
+	if (n <= 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (fibonacci(n - 1) + fibonacci(n - 2));
+}
 
-	first = 1, second = 2;
-	sum = second;
-	next = first + second;
+/**
+ * sum_even_fibonacci - Find the sum of the fibonacci numbers n
+ * @limit: The maximum term of fibonacci
+ * @n: index
+ * @sum: The sum of fibonaccis
+ * Return: Sum
+ */
+unsigned long sum_even_fibonacci(unsigned long limit, int n, unsigned long sum)
+{
+	unsigned long current_fibo = fibonacci(n);
+
+	if (current_fibo >= limit)
+		return (sum);
+	if (current_fibo % 2 == 0)
+		sum = sum + current_fibo;
+	return (sum_even_fibonacci(limit, n + 1, sum));
 }

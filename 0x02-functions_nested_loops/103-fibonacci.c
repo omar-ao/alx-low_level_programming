4d9300bcf65376ccef7fd3b1d3730757c6fb228d
Omar Ahmed O <deeyndiid@gmail.com>
2023-07-18 21:45:02 +0300

    Add 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define MAX_TERM 4000000 /* Max fibonacci term */
+
+/**
+ * main - Entry point
+ * Description: Finds and prints the sum of the even valued terms
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long first, second, next, sum;
+
+	first = 1, second = 2;
+
+	sum = 0;
+
+	while (next < MAX_TERM)
+	{
+		next = first + second;
+		if (next % 2 == 0)
+			sum += next;
+		first = second;
+		second = next;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}

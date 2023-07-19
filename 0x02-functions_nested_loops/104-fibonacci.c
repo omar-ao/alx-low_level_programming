5542ac66aff6e77a432740468746a97c2d6ce31d
Omar Ahmed O <deeyndiid@gmail.com>
2023-07-20 00:50:01 +0300

    Add 104-fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,24 +1,50 @@
 #include <stdio.h>
 
+#define MID 10000000000
+
 /**
  * main - Entry point
+ * Description: prints the first 98 Fibonacci numbers, starting with 1 and 2
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
+	unsigned long first = 0, second = 1, next;
+	unsigned long first_half0, first_half1;
+	unsigned long second_half0, second_half1;
 	int i;
-	unsigned long long first, second, next;
-	first = 1, second = 2;
 
-	printf("%llu, %llu, ", first, second);
-	for (i = 3; i < 98; i++)
+	for (i = 0; i < 92; i++)
 	{
 		next = first + second;
-		printf("%llu", next);
-		if (i < 98)
-			printf(", ");
 		first = second;
 		second = next;
+		printf("%lu, ", next);
+	}
+
+	first_half0 = first / MID;
+	first_half1 = first % MID;
+	second_half0 = second / MID;
+	second_half1 = second / MID;
+
+	for (i = 93 ; i <= 98; i++)
+	{
+		first = first_half0 + first_half1;
+		second = second_half0 + second_half1;
+
+		if (first_half0 + first_half1 > 9999999999)
+		{
+			first += 1;
+			second %= MID;
+		}
+		printf("%lu%lu", first, second);
+		if (i != 98)
+			printf(", ");
+		first_half0 = second_half0;
+		first_half1 = second_half1;
+		second_half0 = first;
+		second_half1 = second;
 	}
 	printf("\n");
 	return (0);

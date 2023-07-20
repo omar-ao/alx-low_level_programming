682e299d1e91b2692c755e2d84c6b8a6bca815e6
Omar Ahmed O <deeyndiid@gmail.com>
2023-07-20 10:39:37 +0300

    Create _isupper function

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase character.
+ * @c: Decimal equivalent of the character to be checked
+ * Return: 1 if c is upper 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (1 * (c >= 65 && c <= 90));
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function definations */
+
+int _isupper(int c);
+
+#endif

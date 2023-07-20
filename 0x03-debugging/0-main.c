aa070188bd13625491ef5da759dd807e5b405805
Omar Ahmed O <deeyndiid@gmail.com>
2023-07-20 20:51:08 +0300

    .

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: Test function that prints if integer is positive or negative
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	positive_or_negative(n);
+}

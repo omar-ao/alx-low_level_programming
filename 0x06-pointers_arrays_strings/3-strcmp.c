65d2c4570d8749d17fa6316990f347e108439d8c
Omar Ahmed O <deeyndiid@gmail.com>
2023-07-27 12:43:39 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: First string
+ * @s2: Second string
+ * Return: Difference of the strings
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, len, diff;
+
+	for (i = 0, len = strlne(s1); i < len; i++)
+	{
+		diff = s1[i] - s2[i];
+		if (diff != 0)
+			break;
+	}
+	return (diff);
+}

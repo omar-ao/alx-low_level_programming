5118835cd40692fafa42fd3840dc127017cb1894
Omar Ahmed O <deeyndiid@gmail.com>
2023-07-27 19:02:50 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,13 +9,11 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, len;
-
-	len = strlen(src);
+	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		*(dest + i) = *(src + i);
-	if (len < n)
+	for ( ; i < n; i++)
 		*(dest + i) = '\0';
 	return (dest);
 }

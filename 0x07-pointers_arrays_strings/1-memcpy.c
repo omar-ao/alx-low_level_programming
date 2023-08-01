69a98dedc9cd64b572e80c674d68d5406deb56b8
Omar Ahmed O <deeyndiid@gmail.com>
2023-08-01 03:08:46 +0300

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,7 +9,13 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	while (n--)
-		*(dest++) = *(src++);
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
 	return (dest);
 }

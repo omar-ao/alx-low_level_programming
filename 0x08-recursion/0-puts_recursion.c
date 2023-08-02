4ab198add97d613003484d08c3d47d5acf6627fa
Omar Ahmed O <deeyndiid@gmail.com>
2023-08-02 04:31:04 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,8 @@
  */
 void _puts_recursion(char *s)
 {
-	char *p = s;
-	_putchar(*p);
-	if (*p == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
-	_puts_recursion(++p);
+	_putchar(*s);
+	if (*s != '\0')
+		_puts_recursion(++s);
+	return;
 }

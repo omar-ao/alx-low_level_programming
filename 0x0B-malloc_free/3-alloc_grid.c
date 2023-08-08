2659f6ead816115c9a33546b890a6bc84475d251
Omar Ahmed O <deeyndiid@gmail.com>
2023-08-08 06:43:03 -0700

    Update the value free_grid takes

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int **alloc_grid(int width, int height)
 	{
 		grid[i] = calloc(height, sizeof(int *));
 		if (grid[i] == NULL)
-			return (free_grid(grid, ++i));
+			return (free_grid(grid, i));
 	}
 	return (grid);
 }

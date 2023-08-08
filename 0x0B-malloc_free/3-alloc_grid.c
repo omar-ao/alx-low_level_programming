e2fa2a300bf19c3bfcb3d65b923e3b86ca3bb8a5
Omar Ahmed O <deeyndiid@gmail.com>
2023-08-08 06:24:33 -0700

    Change size calloc takes

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,12 +12,12 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	grid = malloc(sizeof(int*) * width);
+	grid = malloc(sizeof(int *) * width);
 	if (grid == NULL)
 		return (NULL);
 	for (i = 0; i < width; i++)
 	{
-		grid[i] = calloc(height, sizeof(int *));
+		grid[i] = calloc(height, sizeof(int));
 		if (grid[i] == NULL)
 			return (free_grid(grid, height));
 	}

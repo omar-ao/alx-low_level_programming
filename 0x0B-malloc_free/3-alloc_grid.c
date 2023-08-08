40fcc1fd85d49a9db47f917e86d5a80bda94b761
Omar Ahmed O <deeyndiid@gmail.com>
2023-08-08 06:17:10 -0700

    Add alloc_grid and free_grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * alloc_grid - Returns a pointer to dimensional array
+ * @width: Rows
+ * @height: Column
+ * Return: Pointer
+ */
+int **alloc_grid(int width, int height)
+{
+	int i;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = malloc(sizeof(int *) * width);
+	for (i = 0; i < width; i++)
+	{
+		grid[i] = calloc(height, sizeof(int *));
+		if (grid[i] == NULL)
+			return (free_grid(grid, height));
+	}
+	return (grid);
+}
+/**
+ * free_grid - Frees allocated memory
+ * @grid: 2d array
+ * @n: Height of the array
+ * Return: NULL
+ */
+int **free_grid(int **grid, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		free(grid[i]);
+	free(grid);
+	return (NULL);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,6 +7,8 @@
 #include <stdlib.h>
 
 /* Declarations */
+int **free_grid(int **, int);
+int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);

7fcccdffe43b97012bac757f6cd0bb0b6628bf4a
Omar Ahmed O <deeyndiid@gmail.com>
2023-08-10 21:36:55 +0300

    Add _putchar

---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _putchar - Print a character to standard output
+ * @c: Character to be printed
+ */
+void _putchar(char c)
+{
+	write(1, &c, 1);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Declarations */
+/* Header files */
+#include <unistd.h>
 
+/* Declarations */
+void _putchar(char c);
 #endif

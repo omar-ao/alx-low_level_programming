f72b64d1baae1248372b4e16bb03b9ebcca75a1e
Omar Ahmed Osman <deeyndiid@gmail.com>
2023-08-14 16:08:40 +0300

    Add checks for null pointer

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -5,7 +5,12 @@
  */
 void free_dog(dog_t *d)
 {
-	free(d->name);
-	free(d->owner);
-	free(d);
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
 }

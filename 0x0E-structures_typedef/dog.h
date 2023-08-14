5252ace47e30ff38c77a6f0b92855f5ae7442d8b
Omar Ahmed Osman <deeyndiid@gmail.com>
2023-08-14 12:53:54 +0300

    Add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Defines a new type
+ * @name: Type name
+ * @owner: Type owner
+ * @age: age
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+/* Declarations */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif

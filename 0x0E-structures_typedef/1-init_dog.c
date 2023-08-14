06ae487f6de7e9b66d9549d5321ce2be1606ab7d
Omar Ahmed Osman <deeyndiid@gmail.com>
2023-08-14 13:19:12 +0300

    Add init_dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog - Initialises struct dog
+ * @d: Pointer to struct dog
+ * @name: Pointer the name of the dog
+ * @age: Age of the dog
+ * @owner: Pointer to owner of the dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,4 +12,6 @@ struct dog
 	char *owner;
 	float age;
 };
+/* Initialises struct dog */
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif

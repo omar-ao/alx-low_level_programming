a033bb3f558095579770fe502a4b80f4cffd7920
Omar Ahmed Osman <deeyndiid@gmail.com>
2023-08-14 14:18:10 +0300

    Update init_dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,7 +8,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(*d).name = name;
-	(*d).owner = owner;
-	(*d).age = age;
+	d->name = malloc(strlen(name) + 1);
+	strcpy(d->name, name);
+	d->owner = malloc(strlen(owner) + 1);
+	strcpy(d->owner, owner);
+	d->age = age;
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
+
 #include <stdlib.h>
+#include <string.h>
+
 /**
  * struct dog - Defines a new type
  * @name: Type name

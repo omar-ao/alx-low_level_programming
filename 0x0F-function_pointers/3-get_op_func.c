96ebbf77be25e2eb634d3b728c2e36f0be961ace
Omar Ahmed O <deeyndiid@gmail.com>
2023-08-16 21:48:46 +0300

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,11 +18,11 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (i < 6)
+	while (i < 5)
 	{
 		if (ops[i].op[0] == s[0])
 			return (ops[i].f);
 		i++;
 	}
-	return (NULL);
+	return (ops[5].f);
 }

226411acd4ea20a381795b9d5c1c3963164ca5d3
Omar Ahmed O <deeyndiid@gmail.com>
2023-08-16 21:05:05 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,6 +11,7 @@ int main(int argc, char **argv)
 {
 	int num1, num2, result;
 	int (*cal)(int, int);
+	char op;
 
 	if (argc != 4)
 	{
@@ -20,18 +21,18 @@ int main(int argc, char **argv)
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
+	op = argv[2][0];
+	if (!(op == '+' || op == '-' || op == '*' || op == '/' || op == '%'))
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	if (num2 == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
 	{
 		printf("Error\n");
-		return (99);
+		exit(100);
 	}
-
-	cal = (strlen(argv[2]) == 0) ? NULL :  get_op_func(argv[2]);
-	if (cal == NULL)
-	{
-		printf("Error\n");
-		return (100);
-	}
+	cal = get_op_func(argv[2]);
 	result = cal(num1, num2);
 	printf("%d\n", result);
 	return (0);

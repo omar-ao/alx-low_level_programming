fcc456648a746bcf91d2799e9cd06f1b6beba03b
Omar Ahmed O <deeyndiid@gmail.com>
2023-08-16 23:45:21 +0300

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * main - Entry point
  *
@@ -15,7 +15,7 @@ int main(int argc, char **argv)
 	if (argc != 2)
 	{
 		printf("Error\n");
-		exit(1)
+		exit(1);
 	}
 	n = atoi(argv[1]);
 	if (n < 0)

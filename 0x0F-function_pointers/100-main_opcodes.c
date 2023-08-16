6104ade9c4a10ecb3635ed43f617b69d7b9b725f
Omar Ahmed O <deeyndiid@gmail.com>
2023-08-16 23:43:18 +0300

    update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -15,15 +15,16 @@ int main(int argc, char **argv)
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		exit(1)
 	}
 	n = atoi(argv[1]);
 	if (n < 0)
 	{
 		printf("Error\n");
-		return (2);
+		exit(2);
 	}
 	for (i = 0; i < n; i++)
 		printf("%02x ", *(f + i));
 	printf("\n");
+	return (0);
 }

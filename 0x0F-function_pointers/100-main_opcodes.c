b0e1eabaafec27a2114224dfa1599f1a0ae9e10a
Omar Ahmed O <deeyndiid@gmail.com>
2023-08-16 23:41:05 +0300

    Add 100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: Argument count
+ * @argv: Argument values
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	unsigned char *f = (unsigned char *) main;
+	int i, n;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	for (i = 0; i < n; i++)
+		printf("%02x ", *(f + i));
+	printf("\n");
+}

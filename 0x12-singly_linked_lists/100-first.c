9d4b0da0d062c1b07225f5825a869b97a9fa09a5
Omar Ahmed Osman <deeyndiid@gmail.com>
2023-08-24 16:19:28 +0300

    Add before_main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * before_main - Executes before main and prints string
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

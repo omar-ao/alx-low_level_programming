eeacce9bd2346a629328cddb246b99e904788de1
Omar Ahmed O <deeyndiid@gmail.com>
2023-08-28 23:10:31 +0300

    Update free_listint2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,11 +7,12 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp = *head;
+	listint_t *tmp;
 	listint_t *next;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
+	tmp = *head;
 	while (tmp != NULL)
 	{
 		next = tmp->next;

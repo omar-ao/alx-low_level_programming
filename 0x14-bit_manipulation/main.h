eb8cdc8b19bfe1ca23caa2b5fc76dbb78ae55e69
Omar Ahmed Osman <deeyndiid@gmail.com>
2023-09-06 13:16:29 +0300

    Add print_binary and get_bits

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdlib.h>
+#include <stdio.h>
 unsigned int binary_to_uint(const char *b);
 int _pow(int, int);
 int _strlen(char *);
-
+void print_binary(unsigned long int n);
+int get_bits(unsigned long int);
 #endif

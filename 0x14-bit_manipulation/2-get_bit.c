9a9f9741e93781b568b66b79dd1fcb7cd4e9b2cc
Omar Ahmed Osman <deeyndiid@gmail.com>
2023-09-07 17:19:16 +0300

    Update get_bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,21 +1,5 @@
 #include "main.h"
 /**
- * get_num_bits - Returns the number of bits of a decimal number
- * @n: Decimal number
- * Return: Number of bits
- */
-unsigned int get_num_bits(unsigned long int n)
-{
-	int count = 0;
-
-	while (n > 1)
-	{
-		count++;
-		n >>= 1;
-	}
-	return (count);
-}
-/**
  * get_bit - Returns the value of a bit at a given index
  * @n: Decimal number
  * @index: Index
@@ -23,9 +7,7 @@ unsigned int get_num_bits(unsigned long int n)
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int bits = get_num_bits(n);
-
-	if (index > bits)
+	if (index >= sizeof(n) * 8)
 		return (-1);
 	return ((n & (1 << index)) >> index);
 }

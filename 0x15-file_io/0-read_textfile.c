437996b1ea34795c5217994f0b7f47dea396fb26
Omar Ahmed O <deeyndiid@gmail.com>
2023-09-08 20:47:49 +0300

    Add read_textfile and README.md

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads a text file and printf it to the POSIX
+ * standard output
+ * @filename: Filename
+ * @letters: The number of letters it should read and print
+ * Return: The number of letters it could read or 0 if it fails
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t br, bw;
+	char *buffer;
+	int fd;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(letters);
+	if (buffer == NULL)
+		return (0);
+
+	br = read(fd, buffer, letters);
+	if (br == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	bw = write(STDOUT_FILENO, buffer, br);
+	if (bw == -1 || (br != bw))
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	return (br);
+}

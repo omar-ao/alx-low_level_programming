a2ed4b0efa04c72e2362b529f1df924232f2093a
Omar Ahmed O <deeyndiid@gmail.com>
2023-09-09 05:03:24 +0300

    Add create_file and _strlen and update read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,11 +22,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buffer = malloc(letters);
 	if (buffer == NULL)
+	{
+		close(fd);
 		return (0);
+	}
 
 	br = read(fd, buffer, letters);
 	if (br == -1)
 	{
+		close(fd);
 		free(buffer);
 		return (0);
 	}
@@ -34,9 +38,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	bw = write(STDOUT_FILENO, buffer, br);
 	if (bw == -1 || (br != bw))
 	{
+		close(fd);
 		free(buffer);
 		return (0);
 	}
+	close(fd);
 	free(buffer);
 	return (br);
 }

1ca5484be188752ecde2a6db70b3ce6fedad0d7a
Omar Ahmed O <deeyndiid@gmail.com>
2023-09-28 16:56:15 +0300

    Add print_binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,9 +2,11 @@
 #define MAIN_H
 
 #include <unistd.h>
+#include <stdio.h>
 
 unsigned int binary_to_uint(const char *b);
 int _strlen(const char *);
 unsigned int _pow(int, int);
 int is_valid(const char *);
+void print_binary(unsigned long int n);
 #endif

c306940c2303380a7ec0961cb211bc468cbb9967
Omar Ahmed O <deeyndiid@gmail.com>
2023-11-10 05:55:50 +0300

    update

--- 0x17-doubly_linked_lists/palindrome.py
+++ 0x17-doubly_linked_lists/palindrome.py
@@ -4,11 +4,14 @@ def is_palindrome(num):
     return str(num) == str(num)[::-1]
 
 max_p = 0
+factors = (0, 0)
 
 for i in range(100, 1000):
     for j in range(100, 1000):
         product = i * j
         if is_palindrome(product) and product > max_p:
             max_p = product
+            factors = (i, j)
 
 print(max_p)
+print(factors)

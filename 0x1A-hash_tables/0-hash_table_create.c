c44234f12323ae00747aa1645a5f14ff5cb5d025
Omar Ahmed O <deeyndiid@gmail.com>
2023-12-01 17:09:53 +0300

    Add task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table
+ *
+ * @size: The size of the array
+ * Return: Pointer to a hash table if success or NULL if failed
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = NULL;
+
+	return (ht);
+}

e7ded1b329408b658e7025b5c958db131fd2e2de
Omar Ahmed O <deeyndiid@gmail.com>
2023-12-01 17:32:33 +0300

    Hot fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
 	hash_table_t *ht;
 
 	ht = malloc(sizeof(hash_table_t));
@@ -15,7 +16,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	ht->size = size;
-	ht->array = NULL;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+
+	while (i < size)
+	{
+		ht->array[i] = NULL;
+		i++;
+	}
 
 	return (ht);
 }

2c6034e8ec66b117c711732b7f8ac2542270958f
Omar Osman <deeyndiid@gmail.com>
2024-05-06 09:09:10 +0300

    Add leaner search

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+/* Standard libraries */
+#include "stdio.h"
+
+/* Custom libraries */
+int linear_search(int *array, size_t size, int value);
+
+#endif

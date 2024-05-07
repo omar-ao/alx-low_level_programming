20d572f398bdef2f70395e326bae7d248b01dbdb
Omar Osman <deeyndiid@gmail.com>
2024-05-07 09:58:02 +0300

    Update interpolation_search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -18,7 +18,8 @@ int interpolation_search(int *array, size_t size, int value)
 
 	low = 0;
 	high = size - 1;
-	pos = 0;
+	pos = low + (((double)(high - low) / (array[high] -
+					array[low])) * (value - array[low]));
 
 	while (pos < size)
 	{

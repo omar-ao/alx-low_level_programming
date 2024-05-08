5e0640fbcb9837cea7355589f59fb2218fc7ec4a
Omar Osman <deeyndiid@gmail.com>
2024-05-08 16:56:22 +0300

    Update 0x1E-search_algorithms/100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -11,57 +11,35 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-	int start, end, block = sqrt(size);
+	size_t start, end, block = sqrt(size);
 
 	start = 0;
 	end = block;
 
 	if (array == NULL)
 		return (-1);
+	printf("Value checked array[%ld] = [%d]\n", start, array[start]);
+	if (array[start] == value)
+		return (start);
 
-	while (start < (int) size)
+	while (end < size && array[end] < value)
 	{
-		printf("Value checked array[%d] = [%d]\n", start, array[start]);
-		if (value < array[block])
-		{
-			printf("Value found between indexes [%d] and [%d]\n", start, end);
-			return linear_s(array, start, end, value);
-		}
-		else if (value > array[end])
-		{
-			start = end;
-			end = end + block;
-		}
-		else
-		{
-			printf("Value found between indexes [%d] and [%d]\n", start, end);
-			return (end);
-		}
+		printf("Value checked array[%ld] = [%d]\n", end, array[end]);
+		start = end;
+		end += block;
 	}
-	printf("Value found between indexes [%d] and [%d]\n", start, end);
+	printf("Value found between indexes [%ld] and [%ld]\n", start, end);
+
+	while (start <= end)
+	{
+		printf("Value checked array[%ld] = [%d]\n", start,
+				array[start]);
+		if (array[start] == value)
+			return (start);
+		start++;
+
+		if (end >= size)
+			break;
+	}
 	return (-1);
-}
-
-/**
- * linear_search - Searches for a value in an array of integers using
- * the Linear search algorithm
- *
- * @array: Pointer to array of elements
- * @L:     Start
- * @R:     End
- * @value: Value to be searched
- *
- * Return: Index of the element otherwise
- */
-int linear_s(int *array, int L, int R, int value)
-{
-	int i;
-
-        for (i = L; i <= R; i++)
-        {
-                printf("Value checked array[%d] = [%d]\n", i, array[i]);
-                if (array[i] == value)
-                        break;
-        }
-	return (array[i]);
 }

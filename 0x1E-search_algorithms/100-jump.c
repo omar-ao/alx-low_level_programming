282792f06dc830b468e14b572bbf17a4a3ae396f
Omar Osman <deeyndiid@gmail.com>
2024-05-08 16:58:11 +0300

    Update 0x1E-search_algorithms/100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -18,6 +18,7 @@ int jump_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
+
 	printf("Value checked array[%ld] = [%d]\n", start, array[start]);
 	if (array[start] == value)
 		return (start);
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,7 +10,6 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_sub_array(int *array, int L, int R);
 int jump_search(int *array, size_t size, int value);
-int linear_s(int *array, int L, int R, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
